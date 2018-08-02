75155b4f3e34fe8b8e039488842df6a68468234a
burnmg <rlin1992@hotmail.com>
2018-08-03 00:53:45 +0800

    c

--- NLP/Silicon_Philosopher/lab.py
+++ NLP/Silicon_Philosopher/lab.py
@@ -1,7 +1,25 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+batch_size = 3
+max_length = 3
+hidden_size = 2
+n_layers =1
 
 
+l = [
+    torch.tensor([1,2,3], dtype=torch.long),
+torch.tensor([1,2], dtype=torch.long),
+torch.tensor([1], dtype=torch.long)
 
-s = "c a\tb"
+]
+packed1 = torch.nn.utils.rnn.pad_sequence(l, batch_first=True)
+packed2 = torch.nn.utils.rnn.pad_sequence(l, batch_first=False)
 
-print(s.split('\t'))
+emb = nn.Embedding(4, 5)
+output = emb(packed1)
 
+pack = torch.nn.utils.rnn.pack_padded_sequence(t, lengths = [3,2,1], batch_first= True)
+
+embebedd
--- NLP/Silicon_Philosopher/reader.py
+++ NLP/Silicon_Philosopher/reader.py
@@ -1,21 +1,20 @@
 import os
 import torch
-import torch.tensor as tensor
-
-#     def __init__(self, rnn_type, token_size, embedding_size, hidden_size, layers_size):
-
+import torch.nn as nn
 
 class Corpus(object):
 
     def __init__(self, file_path):
         self.word2idx = {}
         self.idx2word = {}
-        self.idx = self.tokenize(file_path)
+        self.idxs,self.sentences_lengths  = self.tokenize(file_path)
 
     def tokenize(self, file_path):
 
         assert os.path.exists(file_path)
 
+        self.word2idx['<pad>'] = 0
+        self.idx2word[0] = '<pad>'
         with open(file_path, 'r') as f:
             tokens = 0
             for line in f:
@@ -28,10 +27,30 @@ class Corpus(object):
 
                     tokens += 1
 
-        # TODO pad the tensor with different size of sentences.
+
+        idxs_list = []
+        idxs_lengths = []
+        with open(file_path, 'r') as f:
+            tokens = 0
+
+            for line in f:
+
+                words = line.split('\t')[1].lower().split() + ['<eos>']
+                sentence_idxs = torch.zeros(len(words), dtype=torch.long)
+
+                for i, word in enumerate(words):
+                    sentence_idxs[i] = self.word2idx[word]
+
+                idxs_list.append(sentence_idxs)
+                idxs_lengths.append(len(words))
+
+        padded_idxs = nn.utils.rnn.pad_sequence(idxs_list, batch_first=True)
+        return padded_idxs, idxs_lengths
 
 
-path = "/Users/rl/PycharmProjects/pytorch_examples/NLP/Silicon_Philosopher/data/quotes_dataset.txt"
+# path = "/Users/rl/PycharmProjects/pytorch_examples/NLP/Silicon_Philosopher/data/quotes_dataset.txt"
+path = '/Users/rl/PycharmProjects/pytorch_examples/NLP/Silicon_Philosopher/data/small_data'
 c = Corpus(path)
 
+print(c.idxs)
 
